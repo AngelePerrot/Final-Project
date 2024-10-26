6ad73ec3c59548ae3972b0040b02a82db77a0cdb
AngelePerrot <gegelle60@gmail.com>
2024-10-26 10:09:25 +0200

    Update 24/10

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,13 +5,62 @@ import { RouterLink } from 'vue-router'
 
 <template>
   <nav class="navbar">
+    <div class="logo-image">
+      <img src="@/assets/img/file.png" alt="Logo" />
+    </div>
+
     <div class="menu">
       <RouterLink to="/"> Home </RouterLink>
       <RouterLink to="/about"> About </RouterLink>
       <RouterLink to="/contact"> Contacts </RouterLink>
-      <RouterLink to="/Auth"> Login / SignUp </RouterLink>
+    </div>
+
+    <div class="btn-group">
+      <RouterLink to="/Auth">
+        <button type="button" class="btn btn-outline-primary">Sign Up</button>
+        <button type="button" class="btn btn-outline-primary">Log In</button>
+      </RouterLink>
     </div>
   </nav>
 </template>
 
-<style scoped></style>
+<style scoped>
+.navbar {
+  font-size: 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 5px;
+  position: relative;
+  background-color: #e5d5bb;
+  width: 100%;
+  height: 100px;
+}
+
+.logo-image img {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  background-color: white;
+  margin: 10px 0 0 10px;
+}
+
+.menu {
+  display: flex;
+  justify-content: center;
+  flex-grow: 1;
+}
+
+.menu a {
+  margin: 0 20px;
+  text-decoration: none;
+  color: white;
+}
+
+.btn-group {
+  display: flex;
+  justify-content: flex-end;
+  margin-right: 20px;
+  /*color: #65714f;*/
+}
+</style>
